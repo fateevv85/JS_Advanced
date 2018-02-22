a1ce2f10c91ff6c5a4b7403ceaa2f10a0de2a4be
fateevv85 <fateevv85@gmail.com>
2018-02-22 13:55:09 +0300

    create task 2

--- hw4/2 Cities autocomplete/js/citySelect.js
+++ hw4/2 Cities autocomplete/js/citySelect.js
@@ -1,10 +1,17 @@
+//construction for jQuery
 (function ($) {
+  //starts on document.ready
   $(function () {
+    //regular expression for input field
     var $cityRE = /^[a-zа-яё]{3,}$/i;
-    $('#city-range').on('keyup', function () {
+    //var input field
+    var $cityRange = $('#city_range');
+    //add events on keyup in input field
+    $cityRange.on('keyup', function () {
+      //if input text according to reg.expr.
       if ($cityRE.test(this.value)) {
         $(this).parent().removeClass('wrong_input');
-        console.log(this.value);
+        var $citiesArr = [];
         $.ajax({
           url: 'http://api.spacenear.ru/index.php',
           type: 'POST',
@@ -13,6 +20,8 @@
           success: function (cities) {
             $.each(cities, function (index, value) {
               console.log(index + ': ' + value.name);
+              console.log('111');
+              $citiesArr.push(value.name);
             });
             console.log(cities);
             console.log('success');
@@ -20,9 +29,33 @@
           error: function () {
             console.log('cant find that city!');
           }
+        }).done(function () {
+          //if 'city list' non existing, create it
+          if (!$('#city_list').length && $citiesArr.length > 0) {
+            //create 'select' element
+            var $select = $('<select size="5" />').attr('id', 'city_list').hide();
+            for (var i = 0; i < $citiesArr.length; i++) {
+              var $option = $('<option />');
+              $option.attr('value', $citiesArr[i]).text($citiesArr[i]).dblclick(function () {
+                $cityRange.val(this.value);
+                $(this).parent().slideUp(function () {
+                  $(this).remove();
+                });
+              });
+              $select.append($option);
+            }
+            $('#city_input').append($select);
+            //add animation when select menu appearing (city list)
+            $($select).slideDown();
+          }
         });
       } else {
+        //if input text disaccording with reg.expr. add red border and message
         $(this).parent().addClass('wrong_input');
+        //removing city list
+        $('#city_list').slideUp(function () {
+          $(this).remove();
+        });
       }
     });
   });
