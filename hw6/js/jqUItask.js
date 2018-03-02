a0b559ad7348d9a298a1f3e88debd206740d6d6d
fateevv85 <fateevv85@gmail.com>
2018-03-02 21:20:45 +0300

    start working on hw6

--- hw6/js/jqUItask.js
+++ hw6/js/jqUItask.js
@@ -1,5 +1,14 @@
 (function ($) {
   $(function () {
-
+    $('#date').datepicker({
+      firstDay: 1,
+      dateFormat: 'dd.mm.yy',
+      monthNames: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"],
+      dayNamesMin: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"]
+    });
+    $('#dialogW').dialog({
+      width: 100,
+      height: 100
+    });
   });
 })(jQuery);
--- hw6/js/task.js
+++ hw6/js/task.js
@@ -2,10 +2,12 @@
 // При нажатии на кнопку «Отправить» произвести валидацию полей.
 document.querySelector('input[type="submit"]').addEventListener('click', function () {
     var name = document.querySelector('#name');
+    var date = document.querySelector('#date');
     var tel = document.querySelector('#tel');
     var mail = document.querySelector('#mail');
     // var nameRE = /^[A-Za-zА-Яа-яЁё]{2,}$/i;
     var nameRE = /^[a-zа-яё]{2,}$/i;
+    var dateRE = /^\d\d[.,]\d\d[.,]\d{4}$/;
     var telRE = /\+\d{1,4}\(?\d{3}\)?\d{3}-?\d{4}\b/;
     var emailRE = /(^\w+(|(\.|\-)\w+))(?=@[a-z]{2,}\.[a-z]{2,4}\b)/i;
     //e-mail validation with comments
@@ -22,6 +24,18 @@ document.querySelector('input[type="submit"]').addEventListener('click', functio
     function classListToggle(re, data) {
         var test = re.test(data.value);
         if (test) {
+          if (data === date) {
+            var dateArray = date.value.split(/[,.]/);
+            var day = dateArray[0];
+            var month = dateArray[1];
+            var year = dateArray[2];
+            if ((year >= 2010 || year < 1900) || (month > 12 || month < 0) || (day > 31 || day < 0)) {
+              console.log('Wrong date');
+              data.classList.add('false');
+              data.nextElementSibling.classList.remove('hidden');
+              return;
+            }
+          }
             data.classList.remove('false');
             data.nextElementSibling.classList.add('hidden');
         } else {
@@ -33,11 +47,13 @@ document.querySelector('input[type="submit"]').addEventListener('click', functio
     classListToggle(nameRE, name);
     classListToggle(telRE, tel);
     classListToggle(emailRE, mail);
+    classListToggle(dateRE, date);
 
     //button 'reset' clears all classes 'false'
     document.querySelector('input[type="reset"]').addEventListener('click', function () {
         name.classList.remove('false');
         tel.classList.remove('false');
         mail.classList.remove('false');
+        date.classList.remove('false');
     });
 });
