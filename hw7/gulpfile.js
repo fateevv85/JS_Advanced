4e2fa3a034b90d5140666bfd1767cf2ddebfb3e3
fateevv85 <fateevv85@gmail.com>
2018-03-06 15:15:42 +0300

    add image processing, and common task with all another tasks

--- hw7/gulpfile.js
+++ hw7/gulpfile.js
@@ -4,9 +4,12 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     notify = require('gulp-notify'),
     gulpif = require('gulp-if'),
-    minifyCSS = require('gulp-minify-css');
+    minifyCSS = require('gulp-minify-css'),
+    imagemin = require('gulp-imagemin'),
+    del = require('del'),
+    runSequence = require('run-sequence');
 
-gulp.task('project', function () {
+gulp.task('pressFiles', function () {
   return gulp.src('app/*.html')
     .pipe(useref())
     .pipe(gulpif('*.js', uglify()))
@@ -18,3 +21,22 @@ gulp.task('project', function () {
     .pipe(gulp.dest('dist'))
     .pipe(notify('Done!'));
 });
+
+gulp.task('images', function () {
+  gulp.src('app/img/**/*.+(png|jpg|jpeg|gif|svg)')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('clean', function () {
+  del('dist');
+});
+
+gulp.task('clean:dist', function () {
+  del(['dist/**/*', '!dist/img', '!dist/img/**/*']);
+});
+
+gulp.task('build', function () {
+  runSequence('clean', ['pressFiles', 'images']);
+});
+
